5884c5035d163bb0d51fbc61ec467e35665ef2f3
manojchausali <chausali.manoj9@gmail.com>
2021-02-12 21:44:24 +0530

    Delete BoilingVerdict.js

--- src/BoilingVerdict.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-function BoilingVerdict(props){
-	if (props.temprature>100) {
-		return <p>Water will boil</p>
-	}
-	return <p>water will not boil</p>
-}
-export default BoilingVerdict
